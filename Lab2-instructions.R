9421a285fa0a63ec6e9e0968b296746d50127ab3
SankalpOjha <sojha@colgate.edu>
2025-02-06 01:18:09 -0500

    Redoing code

--- Lab2-instructions.R
+++ Lab2-instructions.R
@@ -20,31 +20,19 @@ for(i in 1:length(album)){
   # filter to ensure the files are .wav files
   for(y in 1:length(track.files)){
     songnames = c(songnames, track.files[y])
+    
     song.name.nowav <- c(song.name.nowav, 
                          str_sub(songnames[i], 
                                  start = 1, 
                                  end = (length(songnames[i])-6)))
+    
     full.file.location <- c(full.file.location, paste(file.paths[album[i]], 
                                                 track.files[y],
                                                 sep = "/"))
-  }
-}
-(songnames)
-(full.file.location)
-
-
-
-for(i in 1:length(songnames)){
-  song.name.nowav <- c(song.name.nowav, 
-                       str_sub(songnames[i], 
-                               start = 1, 
-                               end = (length(songnames[i])-6)))
-  
-  for (j in 1:length(song.name.nowav)){
-    #Step C
-    artist <- str_split_i(song.name.nowav[j], "-", 2)
-    album <- str_split_i(full.file.location[j], "/", 3)
-    track <- str_split_i(song.name.nowav[j], "-", 3)
+   
+    artist <- str_split_i(song.name.nowav[y], "-", 2)
+    album <- str_split_i(full.file.location[y], "/", 3)
+    track <- str_split_i(song.name.nowav[y], "-", 3)
     json.artist <- paste(track, ".json", sep = "")
     json.file <- paste(artist, album, json.artist, sep = "-")
     #Step D
@@ -54,6 +42,33 @@ for(i in 1:length(songnames)){
     code.to.process = c(code.to.process, final)
   }
 }
+(songnames)
+(full.file.location)
+
+
+
+#for(i in 1:length(songnames)){
+#i=1
+#  song.name.nowav <- c(song.name.nowav, 
+#                       str_sub(songnames[i], 
+#                               start = 1, 
+#                               end = (length(songnames[i])-6)))
+  
+ # for (j in 1:length(list.files(path = file.paths[album[i]]))){
+  #j=1
+    #Step C
+  #  artist <- str_split_i(song.name.nowav[j], "-", 2)
+   # album <- str_split_i(full.file.location[j], "/", 3)
+    #track <- str_split_i(song.name.nowav[j], "-", 3)
+  #  json.artist <- paste(track, ".json", sep = "")
+  #  json.file <- paste(artist, album, json.artist, sep = "-")
+    #Step D
+   # final <- paste("streaming_extractor_music.exe", " ", 
+    #               '"', full.file.location, '"', " ", '"', json.file,
+     #              '"', sep = "")
+    #code.to.process = c(code.to.process, final)
+  #}
+#}
 (song.name.nowav)
 
 writeLines(code.to.process, "batfile.txt")
